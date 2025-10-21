6b9416f7431689dcece35ee8701eb37b6be63ff4
Yongye Zhu <zyy1102000@gmail.com>
2025-10-21 12:13:36 -0700

    decode bench kv connector

--- vllm/distributed/kv_transfer/kv_connector/v1/__init__.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/__init__.py
@@ -5,4 +5,8 @@ from vllm.distributed.kv_transfer.kv_connector.v1.base import (
     KVConnectorRole,
 )
 
-__all__ = ["KVConnectorRole", "KVConnectorBase_V1"]
+from vllm.distributed.kv_transfer.kv_connector.v1.decode_bench_connector import (  # noqa E:501
+    DecodeBenchConnector,
+)
+
+__all__ = ["KVConnectorRole", "KVConnectorBase_V1", "DecodeBenchConnector"]
